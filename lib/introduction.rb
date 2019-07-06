4a69cda6ceb3ffd15001ed112132b93949419578
Candice Parker <lexus2424@gmail.com>
2019-07-06 22:19:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction-with_language(name, language)
+def introduction_with_language(name, language)
   puts "My name is #{name}. I'm a great #{language} programmer."
 end
